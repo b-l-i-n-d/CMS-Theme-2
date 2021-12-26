a40d9f8957ae7de4d4e1fec819fee9f44618ae94
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-12-27 00:38:40 +0600

    Testimonial

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,13 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".content-swiper", {
     // Optional parameters
     loop: true,
 
     // If we need pagination
     pagination: {
         el: ".swiper-pagination",
-        type: 'progressbar',
-        type: 'bullets',
+        type: "progressbar",
+        type: "bullets",
         clickable: true,
-        dynamicBullets: true
     },
 
     // Navigation arrows
@@ -21,5 +20,44 @@ const swiper = new Swiper(".swiper", {
     autoplay: {
         delay: 1000,
     },
+});
 
+const swiper2 = new Swiper(".testimonial-swiper", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination-testimonial",
+        type: "progressbar",
+        type: "bullets",
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".testimonial-next",
+        prevEl: ".testimonial-prev",
+    },
+
+    // Autoplay
+    autoplay: {
+        delay: 2000,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        576: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 2,
+        },
+    },
 });
