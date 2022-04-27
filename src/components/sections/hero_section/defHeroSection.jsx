d0fa615998cb54fd3c5fbeecfa825d3ffc5321b2
Fahim Faisal <fahim.faisal.abir@gmail.com>
2022-04-27 11:34:55 +0600

    Hero Section

--- src/components/sections/hero_section/defHeroSection.jsx
+++ src/components/sections/hero_section/defHeroSection.jsx
@@ -13,7 +13,7 @@ function DefHeroSection() {
                 <div className="col-sm-9 col-md-8 col-lg-7 d-flex flex-column justify-content-center gap-5">
                     <div className={`${HeroSectionStyles.title} display-4`}>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt,
-                        <span className="text-highlight">deleniti.</span>
+                        <span className="text-highlight"> deleniti.</span>
                     </div>
                     <div className="subtitle h5">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias mollitia
