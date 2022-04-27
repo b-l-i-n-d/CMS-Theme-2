67311a4f761c309172071087dc9149a5afa4349f
Fahim Faisal <fahim.faisal.abir@gmail.com>
2022-04-27 20:17:13 +0600

    ReactBootstrap Implemented

--- src/components/sections/about_section/defAboutSection.jsx
+++ src/components/sections/about_section/defAboutSection.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import AboutImage from '../../../assets/img/about.jpg';
 
 function DefAboutSection() {
     return (
         <section id="about" className="about py-5">
-            <div className="container-xl d-flex gap-3 gap-lg-0 flex-wrap-reverse justify-content-between py-5">
-                <div className="col-12 col-lg-6 d-flex flex-column justify-content-center gap-3 pe-lg-3">
-                    <div className="heading">
-                        <div className="title h3">Lorem, ipsum.</div>
-                        <div className="subtitle display-4">Lorem ipsum dolor sit amet.</div>
-                        <div className="summary">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias,
-                            necessitatibus.
+            <Container className="gap-3 gap-lg-0 flex-wrap-reverse py-5">
+                <Row>
+                    <Col
+                        lg={6}
+                        className="d-flex flex-column justify-content-center gap-3 pe-lg-3 pb-3 pb-lg-0"
+                    >
+                        <div className="heading">
+                            <div className="title h3">Lorem, ipsum.</div>
+                            <div className="subtitle display-4">Lorem ipsum dolor sit amet.</div>
+                            <div className="summary">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias,
+                                necessitatibus.
+                            </div>
                         </div>
-                    </div>
 
-                    <div clasName="h4">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, corrupti
-                        sequi. Maiores facilis eos ut id est nemo aliquid cupiditate.
-                    </div>
-                </div>
-                <div
-                    className="col-12 col-lg-6 img-container"
-                    style={{ backgroundImage: `url(${AboutImage})` }}
-                />
-            </div>
+                        <div clasName="h4">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, corrupti
+                            sequi. Maiores facilis eos ut id est nemo aliquid cupiditate.
+                        </div>
+                    </Col>
+                    <Col
+                        lg={6}
+                        className="img-container"
+                        style={{ backgroundImage: `url(${AboutImage})` }}
+                    />
+                </Row>
+            </Container>
         </section>
     );
 }
--- src/components/sections/hero_section/defHeroSection.jsx
+++ src/components/sections/hero_section/defHeroSection.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import { FaPlayCircle } from 'react-icons/fa';
 import BackgroundImage from '../../../assets/img/hero-img.jpg';
 import HeroSectionStyles from './defHeroSection.module.css';
 
@@ -9,33 +14,38 @@ function DefHeroSection() {
             className={`${HeroSectionStyles.hero} vh-100 img-container`}
             style={{ backgroundImage: `url(${BackgroundImage})` }}
         >
-            <div className="container-xl d-flex flex-wrap mx-auto h-100">
-                <div className="col-sm-9 col-md-8 col-lg-7 d-flex flex-column justify-content-center gap-5">
-                    <div className={`${HeroSectionStyles.title} display-4`}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt,
-                        <span className="text-highlight"> deleniti.</span>
-                    </div>
-                    <div className="subtitle h5">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias mollitia
-                        impedit voluptatem modi atque dicta non eius aliquam quae deserunt.
-                    </div>
-                    <div className="d-inline-flex align-items-center gap-3 gap-md-4 gap-lg-5">
-                        <button
-                            className="btn btn-dark dark-box-shadow rounded-3 border border-2 border-dark text-capitalize"
-                            type="button"
-                        >
-                            Lorem.
-                        </button>
-                        <a
-                            href="##"
-                            className="btn btn-outline-dark dark-box-shadow rounded-3"
-                            type="button"
-                        >
-                            <i className="bx bx-play-circle bx-lg" />
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <Container className="d-flex flex-wrap mx-auto h-100">
+                <Row>
+                    <Col
+                        sm={9}
+                        md={8}
+                        lg={7}
+                        className="d-flex flex-column justify-content-center gap-5"
+                    >
+                        <div className={`${HeroSectionStyles.title} display-4`}>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt,
+                            <span className="text-highlight"> deleniti.</span>
+                        </div>
+                        <div className="subtitle h5">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+                            mollitia impedit voluptatem modi atque dicta non eius aliquam quae
+                            deserunt.
+                        </div>
+                        <div className="d-inline-flex align-items-center gap-3 gap-md-4 gap-lg-5">
+                            <Button className="btn-dark dark-box-shadow rounded-3 border border-2 border-dark text-capitalize">
+                                Lorem.
+                            </Button>
+                            <Button
+                                href="##"
+                                className="btn-outline-dark dark-box-shadow rounded-3"
+                                type="button"
+                            >
+                                <FaPlayCircle size={42} />
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     );
 }
